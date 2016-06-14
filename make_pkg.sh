efbfadc6d5599377894f3261f393f9f36cce62d4
Milan Zink <zeten30@gmail.com>
2016-06-14 23:43:01 +0200

    Documentation update

--- make_pkg.sh
+++ make_pkg.sh
@@ -3,8 +3,6 @@
 find . -name '*.php' -exec dos2unix {} \;
 find . -name '*.xml' -exec dos2unix {} \;
 
-if [ ! -d ./dist ]; then mkdir dist; fi;
-
 version=$(grep '<version>' com_bis.xml | sed -e 's/<[a-z\/]*>//g' | sed -e 's/ *//g')
-rm dist/com_bis*.zip
-zip -r dist/com_bis-${version}-j3X.zip site admin languages com_bis.xml
+rm com_bis*.zip
+zip -r com_bis-${version}-j3X.zip site admin languages com_bis.xml
